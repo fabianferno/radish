8e53ab2cb2257e95a762f4d780d6155c20179701
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-12-29 20:51:03 +0530

    Completed Subgraph

--- subgraph/src/radish-core.ts
+++ subgraph/src/radish-core.ts
@@ -6,8 +6,8 @@ export function handleMarketCreated(event: MarketCreatedEvent): void {
   let entity = new Market(event.params.id.toString());
   entity.question = event.params.question;
   entity.endTime = event.params.endTime;
-  entity.marketContract = event.params.marketContract.toString();
-  entity.creator = event.transaction.from.toString();
+  entity.marketContract = event.params.marketContract;
+  entity.creator = event.transaction.from;
   entity.totalStaked = BigInt.fromI32(0);
   entity.totalYes = BigInt.fromI32(0);
   entity.totalNo = BigInt.fromI32(0);
